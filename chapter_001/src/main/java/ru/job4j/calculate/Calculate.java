1593b761c99d68cf9b5764020c7a0100e6f9bddd
Bulai Nikolai <mailto:turisto777.nb@gmail.com>
2018-08-02 15:58:12 +0300

    1.3. JavDoc. Code convetion[#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+*Calculate вывод строки в консоль.
+*
+*@author Nikolai Bulai (turisto777.nb@gmail.com)
+*/
 public class Calculate {
 
+	/**
+	*Main.
+	*@param args - args.
+	*/
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
