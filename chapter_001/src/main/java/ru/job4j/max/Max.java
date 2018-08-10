c69c678b336718a7f72cc342905419c46628d856
Bulai Nikolai <mailto:turisto777.nb@gmail.com>
2018-08-10 15:03:45 +0300

    4.2. Максимум из двух чисел [#189].

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,21 @@
+package ru.job4j.max;
+
+/**
+ * Max return max number.
+ *
+ * @author Nikolai Bulai (turisto777.nb@gmail.com)
+ * @version 1
+ * @since 10.08.2018
+ */
+public class Max {
+
+    /**
+     * Method max.
+     * @param first integer number.
+     * @param second integer number.
+     * @return a greater number.
+     */
+    public int max(int first, int second) {
+        return (first > second) ? first : second;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * MaxTest.
+ *
+ * @author Nikolai Bulai (turisto777.nb@gmail.com)
+ * @version 1
+ * @since 10.08.2018
+ */
+public class MaxTest {
+
+    /**
+     * Test max.
+     */
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+    }
+}
