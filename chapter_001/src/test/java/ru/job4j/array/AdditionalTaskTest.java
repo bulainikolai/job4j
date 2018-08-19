7e06dd8035c77b5011479df1caebeab4053bb596
Bulai Nikolai <mailto:turisto777.nb@gmail.com>
2018-08-19 22:27:00 +0300

    Дополнительное задание

--- chapter_001/src/test/java/ru/job4j/array/AdditionalTaskTest.java
+++ chapter_001/src/test/java/ru/job4j/array/AdditionalTaskTest.java
@@ -22,9 +22,6 @@ public class AdditionalTaskTest {
         int[] inputFirst = {2, 10, 30, 32, 40};
         int[] inputSecond = {28, 33, 42, 45, 50};
         int[] result = array.getNewArrayFromTwo(inputFirst, inputSecond);
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
-        }
         int[] expect = {2, 10, 28, 30, 32, 33, 40, 42, 45, 50};
         assertThat(result, is(expect));
     }
