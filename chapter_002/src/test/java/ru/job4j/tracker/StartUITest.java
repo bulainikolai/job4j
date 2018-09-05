f50723dc1403d87ba5844837777ebda05b2717a3
Bulai Nikolai <mailto:turisto777.nb@gmail.com>
2018-09-05 22:29:54 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль. [#33585]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,11 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -12,6 +17,34 @@ import static org.junit.Assert.assertThat;
  * @since 03.09.2018
  */
 public class StartUITest {
+
+    /**
+     * Contains object of defaulted stream
+     */
+    private final PrintStream stdout = System.out;
+
+    /**
+     * Contains object of buffer
+     */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    /**
+     * Method loadOutput change from standard stream to buffer
+     */
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    /**
+     * Method backOutput change from buffer to standard stream
+     */
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
 
     /**
      * Test of 0. Add new Item
@@ -95,5 +128,133 @@ public class StartUITest {
         new StartUI(input, tracker).init();
         String[] result = {tracker.getAll()[0].getDescription(), tracker.getAll()[1].getDescription()};
         assertThat(result, is(new String[] {"desc1", "desc2"}));
+    }
+
+    /**
+     * Test of 1. Show all items Using Buffer
+     */
+    @Test
+    public void whenAddOneItemThenShowAllThenGetItInBuffer() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test1", "desc1", System.currentTimeMillis()));
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(
+                new String(this.out.toByteArray()),
+                is(
+                        new StringJoiner(
+                                System.lineSeparator(), "",
+                                System.lineSeparator()
+                        )
+                                .add("Меню.")
+                                .add("0. Add new Item.")
+                                .add("1. Show all items.")
+                                .add("2. Edit item.")
+                                .add("3. Delete item.")
+                                .add("4. Find item by Id.")
+                                .add("5. Find items by name.")
+                                .add("6. Exit Program.")
+                                .add("------------ Отображение списка всех заявок --------------")
+                                .add("1")
+                                .add("ID: " + item.getId())
+                                .add("Name: " + item.getName())
+                                .add("Description: " + item.getDescription())
+                                .add("------------------------------------------------")
+                                .add("Меню.")
+                                .add("0. Add new Item.")
+                                .add("1. Show all items.")
+                                .add("2. Edit item.")
+                                .add("3. Delete item.")
+                                .add("4. Find item by Id.")
+                                .add("5. Find items by name.")
+                                .add("6. Exit Program.")
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Test of 4. Find item by name Using Buffer
+     */
+    @Test
+    public void whenAddOneItemAndFindItByNameThenGetItInBuffer() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test1", "desc1", System.currentTimeMillis()));
+        Input input = new StubInput(new String[]{"5", item.getName(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(
+                new String(this.out.toByteArray()),
+                is(
+                        new StringJoiner(
+                                System.lineSeparator(), "",
+                                System.lineSeparator()
+                        )
+                                .add("Меню.")
+                                .add("0. Add new Item.")
+                                .add("1. Show all items.")
+                                .add("2. Edit item.")
+                                .add("3. Delete item.")
+                                .add("4. Find item by Id.")
+                                .add("5. Find items by name.")
+                                .add("6. Exit Program.")
+                                .add("------------ Поиск заявки по Имени --------------")
+                                .add("1")
+                                .add("ID: " + item.getId())
+                                .add("Name: " + item.getName())
+                                .add("Description: " + item.getDescription())
+                                .add("------------------------------------------------")
+                                .add("Меню.")
+                                .add("0. Add new Item.")
+                                .add("1. Show all items.")
+                                .add("2. Edit item.")
+                                .add("3. Delete item.")
+                                .add("4. Find item by Id.")
+                                .add("5. Find items by name.")
+                                .add("6. Exit Program.")
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Test of 4. Find item by Id Using Buffer
+     */
+    @Test
+    public void whenAddOneItemAndFindItByIdThenGetItInBuffer() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test1", "desc1", System.currentTimeMillis()));
+        Input input = new StubInput(new String[]{"4", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(
+                new String(this.out.toByteArray()),
+                is(
+                        new StringJoiner(
+                                System.lineSeparator(), "",
+                                System.lineSeparator()
+                        )
+                                .add("Меню.")
+                                .add("0. Add new Item.")
+                                .add("1. Show all items.")
+                                .add("2. Edit item.")
+                                .add("3. Delete item.")
+                                .add("4. Find item by Id.")
+                                .add("5. Find items by name.")
+                                .add("6. Exit Program.")
+                                .add("------------ Поиск заявки по ID --------------")
+                                .add("ID: " + item.getId())
+                                .add("Name: " + item.getName())
+                                .add("Description: " + item.getDescription())
+                                .add("------------------------------------------------")
+                                .add("Меню.")
+                                .add("0. Add new Item.")
+                                .add("1. Show all items.")
+                                .add("2. Edit item.")
+                                .add("3. Delete item.")
+                                .add("4. Find item by Id.")
+                                .add("5. Find items by name.")
+                                .add("6. Exit Program.")
+                                .toString()
+                )
+        );
     }
 }
